cdaa08504df962d164529c9995e186b0fb09df17
Promie Yutasane <promie.yutasane@gmail.com>
2021-12-24 06:57:18 +1100

    adding the animation effect to when adding/deleting feedback

--- client/src/components/FeedbackList/FeedbackList.tsx
+++ client/src/components/FeedbackList/FeedbackList.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
 import { IFeedback } from '../../types/feedback'
 import FeedbackItem from '../FeedbackItem'
 
@@ -14,9 +15,22 @@ const FeedbackList: FC<IFeedbackListProps> = ({ feedback, handleDelete }) => {
 
   return (
     <div className="feedback-list">
-      {feedback.map((item: IFeedback) => (
-        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
-      ))}
+      <AnimatePresence>
+        {feedback.map((item: IFeedback) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   )
 }
